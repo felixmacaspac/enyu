fb86fba3391c92b9972b488fef66959b1084dd80
Felix Macaspac <fmcmacaspac@protonmail.com>
2023-01-25 20:58:12 +0800

    feat: add methods example

---
+++ FirstYear/SecondSemester/Methods/numberIdentifier.java
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+pubic class ActivityOne {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Input a number: ");
+
+    int outputNumber = input.nextInt();
+    // call the method
+    result(outputNumber);
+  }
+
+  // method header
+  public static void result(int number) {
+    if (number == 0) {
+      System.out.println(number + " is zero");
+    } else if (number % 2 == 0) {
+      System.out.println(number + " is even");
+    } else {
+      System.out.println(number + " is ");
+    }
+  } // method definition
+}
